cacdf592f01fc5d0c1ebc1590a33464494674104
J. Albert Cruz <jalbertcruz@gmamil.com>
2014-04-12 07:39:30 -0400

    Adding EAIS 2014.

---
+++ 2014/EAIS/concurrent-lps-eais2014/embeberFonts.bat
@@ -0,0 +1,3 @@
+latex concurrent-lps-eais2014.tex
+dvips concurrent-lps-eais2014.dvi
+gsWin64 -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dPDFSETTINGS=/printer -dCompatibilityLevel=1.3 -dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true -sOutputFile=concurrent-lps-eais2014.ps concurrent-lps-eais2014.ps
