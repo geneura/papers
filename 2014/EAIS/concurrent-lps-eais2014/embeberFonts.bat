fcabf0892e3e60ce58d9c5eb39fc12b9d43bfb7c
J. Albert Cruz <jalbertcruz@gmamil.com>
2014-04-12 16:38:04 -0400

    Incluyendo la generacion de las referencias.

--- 2014/EAIS/concurrent-lps-eais2014/embeberFonts.bat
+++ 2014/EAIS/concurrent-lps-eais2014/embeberFonts.bat
@@ -1,3 +1,7 @@
 latex concurrent-lps-eais2014.tex
+bibtex concurrent-lps-eais2014
+latex concurrent-lps-eais2014.tex
+latex concurrent-lps-eais2014.tex
+
 dvips concurrent-lps-eais2014.dvi
-gsWin64 -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dPDFSETTINGS=/printer -dCompatibilityLevel=1.3 -dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true -sOutputFile=concurrent-lps-eais2014.ps concurrent-lps-eais2014.ps
+gsWin64 -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dPDFSETTINGS=/printer -dCompatibilityLevel=1.4 -dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true -sOutputFile=concurrent-lps-eais2014.pdf concurrent-lps-eais2014.ps
