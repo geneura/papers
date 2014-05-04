90b3c147b2927c7cf96c731a8a89712343307afd
JJ Merelo <jjmerelo@gmail.com>
2014-05-04 20:11:13 +0200

    Añadiendo ficheros de R con algunos datos

---
+++ 2014/PPSN/Using Wilcoxon tournamet for noisy fitness/Ruido PLanet Wars/generate-R.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.14;
+use File::Slurp;
+
+my @files=glob "generacion50/*";
+my @dists =  qw(weibull norm gamma);
+for my $f ( @files ) {
+  my $var_name = $f;
+  $var_name =~ s{/}{.};
+  say "noise.$var_name <- read.table('$f')";
+  my $gof = "gof.$var_name <- ";
+  my @gof_variables;
+  
+  for my $d ( @dists ) {
+    my $this_var = "fit.$d.$var_name";
+    say "$this_var <- fitdist( noise.$var_name\$V1, '$d')";
+    push @gof_variables, $this_var;
+  }
+  say "gof.$var_name <- gofstat( list("
+    .join(", ", @gof_variables)."),fitnames=c("
+      .join(", ", map("\"$_\"",@dists))."))";
+  say "summary( gof.$var_name )";
+}
