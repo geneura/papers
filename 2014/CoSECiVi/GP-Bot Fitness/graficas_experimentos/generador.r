76880b107f10094b94280cd99fd428f938d1cb71
Antonio Fernández Ares <antares.es@gmail.com>
2014-05-26 22:26:22 +0200

    Comentado el paper y cambiado nombre del método en las gráficas (TURNS ahora es VICTORIES)

--- 2014/CoSECiVi/GP-Bot Fitness/graficas_experimentos/generador.r
+++ 2014/CoSECiVi/GP-Bot Fitness/graficas_experimentos/generador.r
@@ -15,7 +15,7 @@ s_fitness = s_ex[s_ex[,"IT"]==49,c("EX","BEST_F")]
 #Pintamos los distintos fitness alcanzados en cada ejecución
 pdf('bloxplot_fitness_best_generation_each_execution.pdf')
 par(mfrow=c(1,3))
-boxplot(t_ex[t_ex[,2]==49,"BEST_F"], main="TURNS FITNESS", xlab="Execution", ylab="Fitness")
+boxplot(t_ex[t_ex[,2]==49,"BEST_F"], main="VICTORIES FITNESS", xlab="Execution", ylab="Fitness")
 boxplot(a_ex[a_ex[,2]==49,"BEST_F"], main="AREA FITNESS", xlab="Execution", ylab="Fitness")
 boxplot(s_ex[s_ex[,2]==49,"BEST_F"], main="SLOPE FITNESS", xlab="Execution", ylab="Fitness")
 #title("\nBest Fitness by ",outer=TRUE)
@@ -23,7 +23,7 @@ dev.off()
 
 pdf('plot_fitness_best_generation_each_execution.pdf')
 par(mfrow=c(3,1))
-barplot(t_ex[t_ex[,2]==49,"BEST_F"], main="TURNS FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
+barplot(t_ex[t_ex[,2]==49,"BEST_F"], main="VICTORIES FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
 barplot(a_ex[a_ex[,2]==49,"BEST_F"], main="AREA FITNESS", xlab="Execution", ylab="Fitness",names.arg=a_ex[a_ex[,2]==49,"EX"])
 barplot(s_ex[s_ex[,2]==49,"BEST_F"], main="SLOPE FITNESS", xlab="Execution", ylab="Fitness",names.arg=s_ex[s_ex[,2]==49,"EX"])
 dev.off()
@@ -31,7 +31,7 @@ dev.off()
 
 pdf('bloxplot_fitness_average_generation_each_execution.pdf')
 par(mfrow=c(1,3))
-boxplot(t_ex[t_ex[,2]==49,"AVERAGE_F"], main="TURNS FITNESS", xlab="Execution", ylab="Fitness")
+boxplot(t_ex[t_ex[,2]==49,"AVERAGE_F"], main="VICTORIES FITNESS", xlab="Execution", ylab="Fitness")
 boxplot(a_ex[a_ex[,2]==49,"AVERAGE_F"], main="AREA FITNESS", xlab="Execution", ylab="Fitness")
 boxplot(s_ex[s_ex[,2]==49,"AVERAGE_F"], main="SLOPE FITNESS", xlab="Execution", ylab="Fitness")
 #title("\nBest Fitness by ",outer=TRUE)
@@ -39,21 +39,21 @@ dev.off()
 
 pdf('plot_fitness_best_generation_each_execution.pdf')
 par(mfrow=c(3,1))
-barplot(t_ex[t_ex[,2]==49,"AVERAGE_F"], main="TURNS FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
+barplot(t_ex[t_ex[,2]==49,"AVERAGE_F"], main="VICTORIES FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
 barplot(a_ex[a_ex[,2]==49,"AVERAGE_F"], main="AREA FITNESS", xlab="Execution", ylab="Fitness",names.arg=a_ex[a_ex[,2]==49,"EX"])
 barplot(s_ex[s_ex[,2]==49,"AVERAGE_F"], main="SLOPE FITNESS", xlab="Execution", ylab="Fitness",names.arg=s_ex[s_ex[,2]==49,"EX"])
 dev.off()
 
 pdf('plot_depth_best_generation_each_execution.pdf')
 par(mfrow=c(3,1))
-barplot(t_ex[t_ex[,2]==49,"BEST_DEPTH"], main="TURNS FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
+barplot(t_ex[t_ex[,2]==49,"BEST_DEPTH"], main="VICTORIES FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
 barplot(a_ex[a_ex[,2]==49,"BEST_DEPTH"], main="AREA FITNESS", xlab="Execution", ylab="Fitness",names.arg=a_ex[a_ex[,2]==49,"EX"])
 barplot(s_ex[s_ex[,2]==49,"BEST_DEPTH"], main="SLOPE FITNESS", xlab="Execution", ylab="Fitness",names.arg=s_ex[s_ex[,2]==49,"EX"])
 dev.off()
 
 pdf('plot_age_best_generation_each_execution.pdf')
 par(mfrow=c(3,1))
-barplot(t_ex[t_ex[,2]==49,"BEST_AGE"], main="TURNS FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
+barplot(t_ex[t_ex[,2]==49,"BEST_AGE"], main="VICTORIES FITNESS", xlab="Execution", ylab="Fitness",names.arg=t_ex[t_ex[,2]==49,"EX"])
 barplot(a_ex[a_ex[,2]==49,"BEST_AGE"], main="AREA FITNESS", xlab="Execution", ylab="Fitness",names.arg=a_ex[a_ex[,2]==49,"EX"])
 barplot(s_ex[s_ex[,2]==49,"BEST_AGE"], main="SLOPE FITNESS", xlab="Execution", ylab="Fitness",names.arg=s_ex[s_ex[,2]==49,"EX"])
 dev.off()
@@ -172,7 +172,7 @@ dev.off()
 pdf('boxplot_victoria_bot_por_metodo.pdf')
 par(mfrow=c(1,3))
 boxplot(as.matrix(table(batallas[batallas["Type"]=="TURNS","Wins"],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20"))/1134*100),ylim=c(0,100))
-title(main="TURNS FITNESS",ylab="% Victories")
+title(main="VICTORIES FITNESS",ylab="% Victories")
 
 boxplot(as.matrix(table(batallas[batallas["Type"]=="AREA","Wins"],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","t_0","t_1","t_2","t_2","t_3","t_4","t_5","t_6","t_7","t_8","t_9","t_10","t_11","t_12","t_13","t_14","t_15","t_16","t_17","t_18","t_19","t_20"))/1134*100),ylim=c(0,100))
 title(main="AREA FITNESS")
@@ -193,6 +193,33 @@ sink(file="tabla_victorias_por_bot.latex")
 xtable(table(batallas[,"Wins"])/nrow(batallas)*100)
 dev.off()
 
+#VICTORIAS POR TIPO DE MAPA
+levelplot(as.matrix(table(batallas[batallas["Type"]=="TURNS",c("Wins","Map")],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20")  )),col.regions=grey(100:0/100))
+
+#Superficies por método, bot y mapa
+levelplot(as.matrix(table(batallas[batallas["Type"]=="TURNS",c("Wins","Map")],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20"))),col.regions=grey(100:0/100))
+#title(main="VICTORIES FITNESS",sub="Victories")
+
+levelplot(as.matrix(table(batallas[batallas["Type"]=="AREA",c("Wins","Map")],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","t_0","t_1","t_2","t_2","t_3","t_4","t_5","t_6","t_7","t_8","t_9","t_10","t_11","t_12","t_13","t_14","t_15","t_16","t_17","t_18","t_19","t_20"))),col.regions=grey(100:0/100))
+#title(main="AREA FITNESS")
+
+levelplot(as.matrix(table(batallas[batallas["Type"]=="SLOPE",c("Wins","Map")],exclude=c("DRAW","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20","t_0","t_1","t_2","t_2","t_3","t_4","t_5","t_6","t_7","t_8","t_9","t_10","t_11","t_12","t_13","t_14","t_15","t_16","t_17","t_18","t_19","t_20"))),col.regions=grey(100:0/100))
+#title(main="SLOPE FITNESS")
+rm(t_l)
+rm(t_a)
+rm(t_s)
+dev.off()
+
+#Tabla victorias método por tipo de mapa
+
+      sink(file="tabla_victorias_por_mapa.latex")
+      xtable(table(batallas[,c("Type","Map")])/3969)
+      dev.off()
+
+vsGenebot = read.csv("datos/VsGenebot_n.csv")
+
+
+
 
 
 
@@ -200,7 +227,7 @@ dev.off()
 pdf('boxplot_victoria_bot_por_metodo_contra_genebot.pdf')
 par(mfrow=c(1,3))
 boxplot(as.matrix(table(vsGenebot[vsGenebot["Type"]=="t","Wins"],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20"))/900*100),ylim=c(0,100))
-title(main="TURNS FITNESS",ylab="% Victories")
+title(main="VICTORIES FITNESS",ylab="% Victories")
 
 boxplot(as.matrix(table(vsGenebot[vsGenebot["Type"]=="a","Wins"],exclude=c("DRAW","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","t_0","t_1","t_2","t_2","t_3","t_4","t_5","t_6","t_7","t_8","t_9","t_10","t_11","t_12","t_13","t_14","t_15","t_16","t_17","t_18","t_19","t_20"))/900*100),ylim=c(0,100))
 title(main="AREA FITNESS")
@@ -216,7 +243,7 @@ levelplot(as.matrix(table(batallas[batallas["Wins"]=="DRAW",1:2])),col.regions=g
 pdf('boxplot_victoria_bot_por_metodo_contra_genebot.pdf')
 par(mfrow=c(1,3))
 table(vsGenebot[vsGenebot["Type"]=="t","Wins"],exclude=c("t_0","t_1","t_2","t_2","t_3","t_4","t_5","t_6","t_7","t_8","t_9","t_10","t_11","t_12","t_13","t_14","t_15","t_16","t_17","t_18","t_19","t_20","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20"))/900*100
-title(main="TURNS FITNESS",ylab="% Victories")
+title(main="VICTORIES FITNESS",ylab="% Victories")
 
 table(vsGenebot[vsGenebot["Type"]=="t","Wins"],exclude=c("t_0","t_1","t_2","t_2","t_3","t_4","t_5","t_6","t_7","t_8","t_9","t_10","t_11","t_12","t_13","t_14","t_15","t_16","t_17","t_18","t_19","t_20","s_0","s_1","s_2","s_2","s_3","s_4","s_5","s_6","s_7","s_8","s_9","s_10","s_11","s_12","s_13","s_14","s_15","s_16","s_17","s_18","s_19","s_20","a_0","a_1","a_2","a_2","a_3","a_4","a_5","a_6","a_7","a_8","a_9","a_10","a_11","a_12","a_13","a_14","a_15","a_16","a_17","a_18","a_19","a_20"))/900*100
 title(main="AREA FITNESS")
